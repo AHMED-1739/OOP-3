b5e698c403747cf74d8fc210e277dcf616d2b876
AHMED-1739 <a85459866@gmail.com>
2024-11-16 00:40:41 +0300

    remove something

--- Program.cs
+++ Program.cs
@@ -20,23 +20,21 @@ namespace OOP_3
          
             List<Student> list = new List<Student>();
             while (true)
-            {
-                
+            {  
                 if (counter >= 1)
                 {
                     Console.WriteLine("do you want to insert another student? \n Yes or NO ");
                Re_input:    string input = Console.ReadLine();
                   input = input.ToLower();
                     if (input == "yes")
-                        goto insert;
+                        goto insert; //line38
                     else if (input == "no")
                         break;
                     else
                     {
-                        Console.WriteLine("what you want? Choose to write yes or no please"); goto Re_input;
+                        Console.WriteLine("what you want? Choose to write yes or no please"); goto Re_input;//line 27
                     }
                 }
-
             insert: Console.WriteLine("enter the information of the student please:");
 
                 Console.Write("the name: ");
@@ -53,7 +51,7 @@ namespace OOP_3
                 if(mark <0&& mark>100)
                 {
                     Console.WriteLine("can you enter a logical degree?");
-                    goto insert_mark;
+                    goto insert_mark;   //line 50
                 }
                 list.Add(new Student { Id = id, Name = name, Mark = mark});
 
@@ -69,11 +67,6 @@ namespace OOP_3
                     Console.WriteLine("Student Name: {0}\nStudent ID: {1}\nStudent Mark: {2}", student.Name, student.Id, student.Mark);
                     Console.WriteLine();
                 }
-
-
-
-
-
         }
     }
 }
