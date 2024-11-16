a206e30867e3d3dd46c5ba0d86f0c1719accbde9
AHMED-1739 <a85459866@gmail.com>
2024-11-16 22:44:07 +0300

    maybe the last change

--- Program.cs
+++ Program.cs
@@ -31,7 +31,10 @@ namespace OOP_3
             User_Email = Account.UserEmail;
             User_Password =Account.UserPassword;
         }
-        
+        public void Change_Password(string new_pass)
+        {
+            this.User_Password = new_pass;
+        }
         static public UserAccount Check_Account(UserAccount account, string pass, string userName)
         {
             if (account.UserPassword == pass && account.UserName == userName)
@@ -39,6 +42,7 @@ namespace OOP_3
 
                 return null;
         }
+        
 
     }
     internal class Program
@@ -46,12 +50,13 @@ namespace OOP_3
   
         static void Main(string[] args)
         {
+
             ArrayList Account_List = new ArrayList();
             // create some account
             char c = '1';
             for (int i = 0; i < 5; i++)
             {
-                Account_List.Add(new UserAccount("Test_Name " + c, "Test_Pass " + c, $" Test_Email {c} @email.com"));
+                Account_List.Add(new UserAccount("Test_Name" + c, "Test_Pass" + c, $" Test_Email{c}@email.com"));
                  c++;
             }
             Console.WriteLine("Enter information to verify");
@@ -60,36 +65,41 @@ namespace OOP_3
             Console.Write("Password: ");
             string pass= Console.ReadLine();
 
-            UserAccount UserAccount;
+            UserAccount UserAccount=null;
 
             for (int i = 0; i < Account_List.Count; i++)
             {
                 UserAccount Account = (UserAccount)Account_List[i];
                 if (UserAccount.Check_Account(Account, pass, userName) != null)
                 {
-                    Console.WriteLine("Information verified.");
-                    UserAccount = UserAccount.Check_Account(Account,pass,userName);
+                    Console.WriteLine("Information verified.\n");
+                    UserAccount = new UserAccount(UserAccount.Check_Account(Account,pass,userName));
+                    break;
+                } 
+                if(i==Account_List.Count - 1)
+                    Console.WriteLine("The information is invalid");
+            }
+            if (UserAccount != null)
+            {
+                Console.WriteLine("Do you want to change your password ? yes or no");
+              input: string input = Console.ReadLine();
+                input = input.ToLower();
+                if (input == "yes")
+                {
+                    Console.Write("Enter the new password: ");
+                    string new_pass = Console.ReadLine();
+                    UserAccount.Change_Password(new_pass);
+                }
+                else if (input == "no")
+                    Console.WriteLine("OK");
+                else 
+                { 
+                    Console.WriteLine("choss yes or no!");
+                    goto input;
                 }
             }
-            Console.WriteLine("done");
-
-            /*    Console.WriteLine("Do you want to change your password ? yes or no");
-                string input=Console.ReadLine();
-                input=input.ToLower();
-                if(input =="yes")
-                {
-                    Console.Write("enter the new password: ");
-                    string new_pass = Console.ReadLine();
-                    UserAccount.Chan
-
-
-                }
-    */
-
-
-
-
-
+            Console.WriteLine("Information after change:");
+            Console.WriteLine("Name: {0}\nPassword:{1}",UserAccount.UserName,UserAccount.UserPassword);
         }
     }
 }
