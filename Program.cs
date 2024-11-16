ac06510cae56682c3b37f69f58ba0be0bec7ef6a
AHMED-1739 <a85459866@gmail.com>
2024-11-16 21:56:56 +0300

    Q2

--- Program.cs
+++ Program.cs
@@ -1,77 +1,95 @@
 ﻿using System;
+using System.CodeDom;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OOP_3
 {
-
-    class Student
+    class UserAccount
     {
-        public double Mark;
-        public string Name, Id;
+        private string User_Name, User_Password, User_Email;
+
+        public string UserName => User_Name;
+        public string UserPassword => User_Password;
+        public string UserEmail => User_Email;
+
+        //this constructor to create new Account
+        public UserAccount(string User_Name, string User_Password, string User_Email)
+        {
+            this.User_Name = User_Name;
+            this.User_Password = User_Password;
+            this.User_Email = User_Email;
+        }
+        public UserAccount(UserAccount Account)
+        {
+            User_Name=Account.UserName;
+            User_Email = Account.UserEmail;
+            User_Password =Account.UserPassword;
+        }
+        
+        static public UserAccount Check_Account(UserAccount account, string pass, string userName)
+        {
+            if (account.UserPassword == pass && account.UserName == userName)
+                 return account;
+
+                return null;
+        }
+
     }
     internal class Program
     {
+  
         static void Main(string[] args)
         {
-            int counter = 0;
-         
-            //is it work?
-            //will see 
-
-
-
-            List<Student> list = new List<Student>();
-            while (true)
-            {  
-                if (counter >= 1)
-                {
-                    Console.WriteLine("do you want to insert another student? \n Yes or NO ");
-               Re_input:    string input = Console.ReadLine();
-                  input = input.ToLower();
-                    if (input == "yes")
-                        goto insert; //line38
-                    else if (input == "no")
-                        break;
-                    else
-                    {
-                        Console.WriteLine("what you want? Choose to write yes or no please"); goto Re_input;//line 27
-                    }
-                }
-            insert: Console.WriteLine("enter the information of the student please:");
-
-                Console.Write("the name: ");
-
-                string name= Console.ReadLine();
-
-                Console.WriteLine("the ID: ");
-
-                string id= Console.ReadLine();  
-                
-                Console.WriteLine("the mark:");
-
-                insert_mark: double mark = Convert.ToDouble(Console.ReadLine());
-                if(mark <0&& mark>100)
-                {
-                    Console.WriteLine("can you enter a logical degree?");
-                    goto insert_mark;   //line 50
-                }
-                list.Add(new Student { Id = id, Name = name, Mark = mark});
-
-                counter++;
-                if (counter == 10)
-                    break;
+            ArrayList Account_List = new ArrayList();
+            // create some account
+            char c = '1';
+            for (int i = 0; i < 5; i++)
+            {
+                Account_List.Add(new UserAccount("Test_Name " + c, "Test_Pass " + c, $" Test_Email {c} @email.com"));
+                 c++;
             }
-            Console.Clear();
-            Console.WriteLine("Successful students information: \n");
-            foreach (Student student in list)
-                if (student.Mark >= 50)
+            Console.WriteLine("Enter information to verify");
+            Console.Write("User Name: ");
+            string userName = Console.ReadLine();
+            Console.Write("Password: ");
+            string pass= Console.ReadLine();
+
+            UserAccount UserAccount;
+
+            for (int i = 0; i < Account_List.Count; i++)
+            {
+                UserAccount Account = (UserAccount)Account_List[i];
+                if (UserAccount.Check_Account(Account, pass, userName) != null)
                 {
-                    Console.WriteLine("Student Name: {0}\nStudent ID: {1}\nStudent Mark: {2}", student.Name, student.Id, student.Mark);
-                    Console.WriteLine();
+                    Console.WriteLine("Information verified.");
+                    UserAccount = UserAccount.Check_Account(Account,pass,userName);
                 }
+            }
+            Console.WriteLine("done");
+
+            /*    Console.WriteLine("Do you want to change your password ? yes or no");
+                string input=Console.ReadLine();
+                input=input.ToLower();
+                if(input =="yes")
+                {
+                    Console.Write("enter the new password: ");
+                    string new_pass = Console.ReadLine();
+                    UserAccount.Chan
+
+
+                }
+    */
+
+
+
+
+
         }
     }
 }
